1221268c5f2fcf99b22ad80f28f5e01f965a569d
Sarahi Villegas <sg.villegasq@gmail.com>
2021-06-27 00:26:28 +0100

    added pic2folder class

---
+++ main.py
@@ -0,0 +1,4 @@
+from pic2folder import Pic2Folder
+
+pic2folder = Pic2Folder()
+pic2folder.mainloop()
---
+++ pic2folder.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+from tkinter import mainloop, ttk
+
+
+class Pic2Folder(tk.Tk):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.title('Pic 2 Folder')
+
+        mainframe = ttk.Frame(self, padding='3 3 12 12')
+        mainframe.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.S, tk.E))
+
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+
+        title = ttk.Label(mainframe, text='<folder title>')
+        title.grid(column=1, row=1)
+
+        # add gallery
+
+        # add folder/options section
+
+        # add sidebar
